11737ba07d05a10c24636ab71c910ac17bb8f4e0
Aghilas Y. Boussaa <ayb@3hg.fr>
2023-11-03 17:49:33 +0100

    Update monk.js

--- monk.js
+++ monk.js
@@ -1,4 +1,11 @@
-blocked = await browser.storage.sync.get('blocked');
-if (blocked.includes(window.location.hostname)) {
-    document.body.remove()
-}
+browser.storage.local.get(data => {
+    console.log(data);
+  if (data.blocked) {
+      blocked = data.blocked;
+      console.log(data.blocked);
+      console.log(blocked.split("\n"));
+      if (blocked.split("\n").includes(window.location.hostname)) {
+          document.body.remove()
+      }
+  }
+});
